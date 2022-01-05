c0c62a43a7ad5ea743c0107dd0154cc04ede0ac6
setrar <setrar@valiha.com>
2022-01-05 15:59:03 -0500

    .

--- 1.Docker/300116670/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -l
-
-sh -c "echo Bonjour tout le monde mon nom est $Auriane"
--- 1.Docker/300117782/event.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "########################## LA DATE D'AUJOURD'HUI";
-echo `date` $@ >> log.txt;
-cat log.txt;
