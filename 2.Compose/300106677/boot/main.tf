07f96ea3e624a578e2e12bf038161412185a2540
Leonaldo Decimus <leonaldo_decimus@yahoo.com>
2022-03-21 16:14:00 -0500

    .

--- 2.Compose/300106677/boot/main.tf
+++ 2.Compose/300106677/boot/main.tf
@@ -89,7 +89,7 @@ resource "azurerm_linux_virtual_machine" "CB-AZ-300106677" {
   name                = "machine-CB-AZ-300106677"
   resource_group_name = azurerm_resource_group.CB-AZ-300106677.name
   location            = azurerm_resource_group.CB-AZ-300106677.location
-  size                = "Standard_B1ls"
+  size                = "Standard_B2s"
   admin_username      = "ubuntu"
   network_interface_ids = [
     azurerm_network_interface.CB-AZ-300106677.id,
